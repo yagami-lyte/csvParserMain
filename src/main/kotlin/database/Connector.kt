542eea4fb0a6ef3aa1c5351d9a8ff18b1111e145
yagami-lyte <usvisen2000@gmail.com>
2022-06-06 17:29:07 +0530

    Added id and password for database

--- src/main/kotlin/database/Connector.kt
+++ src/main/kotlin/database/Connector.kt
@@ -6,8 +6,8 @@ import java.sql.DriverManager
 
 class Connector : DatabaseConnector {
     override fun makeConnection(): Connection {
-        val user = "postGre"
-        val password = "postGre"
-        return DriverManager.getConnection("postgres://vcbmkareybcfuj:1472a5b4872eff85846616a7cefeadb5a8c11faa5e838a5e663778ecb7f1b00f@ec2-54-147-33-38.compute-1.amazonaws.com:5432/d5thkls1el9a60")
+        val user = "vcbmkareybcfuj"
+        val password = "1472a5b4872eff85846616a7cefeadb5a8c11faa5e838a5e663778ecb7f1b00f"
+        return DriverManager.getConnection("jdbc:postgresql://ec2-54-147-33-38.compute-1.amazonaws.com:5432/d5thkls1el9a60",user,password)
     }
 }
